52848ebeeb09fc24e30d6891131bba9c49171e15
Jonah Larsen <Jonah.Larsen@gmail.com>
2025-11-07 14:32:34 -0700

    app component

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+  return (
+    <>
+        <header>
+            <h1>TIC TAC TOE</h1>
+            <nav>
+                <ul className="navbar">
+                    <li><a className="navbar_item" href="index.html">Play</a></li>
+                    <li><a className="navbar_item" href="login.html">Login</a></li>
+                    <li><a className="navbar_item active" href="games.html">Games</a></li>
+                </ul>
+            </nav>
+        </header>
+
+        <main>hello</main>
+
+        <footer>
+            <hr />
+            <span><a href="https://github.com/JonahLarsen/startup">Github Repo: https://github.com/JonahLarsen/startup</a></span>
+            <br />
+            <span>Created by Jonah Larsen</span>
+        </footer>
+    </>
+  );
 }
