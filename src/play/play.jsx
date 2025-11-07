ee65a5b76468a326ddbb99539c38ab6f32ad79e0
Jonah Larsen <Jonah.Larsen@gmail.com>
2025-11-07 15:00:35 -0700

    Play component

--- src/play/play.jsx
+++ src/play/play.jsx
@@ -2,6 +2,25 @@ import React from "react";
 
 export function Play() {
     return (
-        <main>Play</main>
+        <main>
+            <h2>Game</h2>
+            <p>Logged in as Jonah</p>
+            <h3>ID: 1 |  Opponent: Bob McDonald</h3>
+            <form>
+                <label for="gamechoice">Choose game:</label>
+                <select name="gamechoice" id="gamechoice">
+                    <option value="1">1: Bob McDonald</option>
+                    <option value="2">2: Billy Joel</option>
+                    <option value="3">3: Jimbo James</option>
+                </select>
+            </form>
+            <img src="../public/img/placeholder_game.png" alt="Placeholder image for Tic Tac Toe game" />
+            <p>Your turn...</p>
+            <br />
+            <br />    
+            <p>Need help? Ask ChatGPT for the best next move!</p>
+            <button>Ask ChatGPT</button>
+            <p>The best move based on your current situation would be...</p>
+        </main>
     )
 }
