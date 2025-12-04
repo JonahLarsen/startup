05f245ac450c80faee60abe4ef070842d5a96233
Jonah Larsen <Jonah.Larsen@gmail.com>
2025-12-03 19:04:58 -0700

    test connection

--- service/database.js
+++ service/database.js
@@ -9,3 +9,16 @@ const db = client.db('rental');
 const games = db.collection('games');
 const users = db.collection('users');
 const gamesStatus = db.collection('gamesStatus');
+
+(async function testConnection() {
+    try {
+        await db.command({ping: 1});
+        console.log("Connect to database");
+    } catch (ex) {
+        console.log("Unable to connect, error:", ex.message);
+    }
+})();
+
+function getUser(email){
+    return users.findOne({email: email});
+}
