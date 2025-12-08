2908d8c9515ef2fc10e77ac2ad6a010695b49915
Jonah Larsen <Jonah.Larsen@gmail.com>
2025-12-07 23:21:48 -0700

    Collect and send vote

--- src/games/gameVotes.jsx
+++ src/games/gameVotes.jsx
@@ -1,25 +1,48 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import React from "react";
+import { websocketClient } from "./websocketClient";
 
-export function GameVotes(props) {
+export function GameVotes() {
+    const [tallies, setTallies] = useState({
+                                            TopLeft: 0,
+                                            TopMiddle: 0,
+                                            TopRight: 0,
+                                            MiddleLeft: 0,
+                                            MiddleMiddle: 0,
+                                            MiddleRight: 0,
+                                            BottomLeft: 0,
+                                            BottomMiddle: 0,
+                                            BottomRight: 0
+                                        });
+
+    useEffect(() => {  
+        const webSocket = new websocketClient(setTallies);
+
+        return () => {
+            webSocket.socket.close();
+        }
+    }, [])
+
 
     function sendVote() {
-        props.webSocket
+        const selectedValue = document.getElementById('startSpot').value;
+
+        webSocket.sendVote(selectedValue);
     }
     
     return (
         <div>
             <h3>Vote on the best starting spot</h3>
             <select name="spot" id="startSpot">
-                <option value="Top-Left">Top Left</option>
-                <option value="Top-Middle">Top Middle</option>
-                <option value="Top-Right">Top Right</option>
-                <option value="Middle-Left">Middle Left</option>
-                <option value="Middle-Middle">Middle Middle</option>
-                <option value="Middle-Right">Middle Right</option>
-                <option value="Bottom-Left">Bottom Left</option>
-                <option value="Bottom-Middle">Bottom Middle</option>
-                <option value="Bottom-Right">Bottom Right</option>
+                <option value="TopLeft">Top Left</option>
+                <option value="TopMiddle">Top Middle</option>
+                <option value="TopRight">Top Right</option>
+                <option value="MiddleLeft">Middle Left</option>
+                <option value="MiddleMiddle">Middle Middle</option>
+                <option value="MiddleRight">Middle Right</option>
+                <option value="BottomLeft">Bottom Left</option>
+                <option value="BottomMiddle">Bottom Middle</option>
+                <option value="BottomRight">Bottom Right</option>
             </select>
             <button onClick={sendVote}>Send Vote</button>
         </div>
